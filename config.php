1c6086329f49b2042b5a216816538f25481aae92
沈觅仁 <dickeny@gmail.com>
2010-11-27 01:29:22 +0800

    v1版。及其简陋版本

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('CONSUMER_KEY', 'SFtcUBB5IImkmpff2ezVg');
+define('CONSUMER_SECRET', 'MXMSnSTJMgUYWEE1vMLy9tVqJHK6BcoszCwKSJ0xkL0');
+define('OAUTH_TOKEN', '100175837-tiSERr5kns1C5QZrxN1iaZyH4VGtvEsbQGfPn5Bm');
+define('OAUTH_TOKEN_SECRET', 'S2qRAOtmvIPrhHvstNYUl82cOClxcZyMRRHuoKplE');
+
+?>
+
---
+++ toap.php
@@ -0,0 +1,37 @@
+<?php
+
+require('config.php');
+require('include/twitteroauth.php');
+
+function get_twitter_op(){
+    $doc = "^".$_SERVER['DOCUMENT_URI'];
+    $uri = "^".$_SERVER['REQUEST_URI'];
+
+    if (substr($doc, -1) !== "/") {
+        $doc .= "$";
+    } else {
+        $doc = substr($doc, 0, -1) . '$';
+    }
+    $base_doc = str_replace('index.php$', '', $doc);
+    $op = str_replace($base_doc, '', $uri);
+    $op .= ".json";
+    return explode('.', $op);
+}
+
+
+date_default_timezone_set('GMT-8');
+$parameters = array();
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $parameters = $_GET;
+} else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $parameters = $_POST;
+}
+
+$args = get_twitter_op();
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
+$connection->decode_json = false;
+$connection->format = $args[1];
+$response = $connection->post($args[0], $parameters);
+echo $response;
+
+?>
