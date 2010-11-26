47cbade82d855d84a8aecb16cbae3c9378cbbf41
沈觅仁 <dickeny@gmail.com>
2010-11-27 01:51:34 +0800

    支持多数API proxy

--- config.php
+++ config.php
@@ -5,5 +5,5 @@ define('CONSUMER_SECRET', 'MXMSnSTJMgUYWEE1vMLy9tVqJHK6BcoszCwKSJ0xkL0');
 define('OAUTH_TOKEN', '100175837-tiSERr5kns1C5QZrxN1iaZyH4VGtvEsbQGfPn5Bm');
 define('OAUTH_TOKEN_SECRET', 'S2qRAOtmvIPrhHvstNYUl82cOClxcZyMRRHuoKplE');
 
+#结尾不要有空行~~
 ?>
-
--- toap.php
+++ toap.php
@@ -6,12 +6,12 @@ require('include/twitteroauth.php');
 function get_twitter_op(){
     $doc = "^".$_SERVER['DOCUMENT_URI'];
     $uri = "^".$_SERVER['REQUEST_URI'];
-
     if (substr($doc, -1) !== "/") {
         $doc .= "$";
     } else {
         $doc = substr($doc, 0, -1) . '$';
     }
+
     $base_doc = str_replace('index.php$', '', $doc);
     $op = str_replace($base_doc, '', $uri);
     $op .= ".json";
@@ -19,19 +19,18 @@ function get_twitter_op(){
 }
 
 
-date_default_timezone_set('GMT-8');
-$parameters = array();
-if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-    $parameters = $_GET;
-} else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $parameters = $_POST;
-}
-
+#date_default_timezone_set('GMT-8');
+$response = "request method is not support.";
 $args = get_twitter_op();
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
 $connection->decode_json = false;
 $connection->format = $args[1];
-$response = $connection->post($args[0], $parameters);
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $response = $connection->get($args[0], $_GET);
+} else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $response = $connection->post($args[0], $_POST);
+}
+
 echo $response;
 
 ?>
